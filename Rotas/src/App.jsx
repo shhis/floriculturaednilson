5f162aec71533c8c40d1f60993c782c175f2b798
thomas <110392119+shhis@users.noreply.github.com>
2023-04-24 10:17:26 -0300

    Add files via upload

--- Rotas/src/App.jsx
+++ Rotas/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home.jsx";
 import Contato from "./pages/Contato.jsx";
 import Sobre from "./pages/Sobre.jsx";
+import Produtos from "./pages/Produtos.jsx" 
 
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
@@ -13,6 +14,7 @@ function App(){
         <Route path="/" element={<Home/>}/>
         <Route path="/sobre" element={<Sobre/>}/>
         <Route path="/contato" element={<Contato/>}/>
+        <Route path="/produtos" element={<Produtos/>}/>
       </Routes>
       </BrowserRouter>
 
