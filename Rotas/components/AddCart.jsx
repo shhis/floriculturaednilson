221af3e469f1f5d7211539f7a34352e9c951b4d1
thomas <110392119+shhis@users.noreply.github.com>
2023-04-24 11:22:41 -0300

    Add files via upload

---
+++ Rotas/components/AddCart.jsx
@@ -0,0 +1,10 @@
+function AddCart(){
+    return(
+        <div>
+            <a>Adicionar ao carrinho</a>&nbsp;&nbsp;&nbsp;&nbsp;
+            <button>+</button>
+        </div>
+    )
+}
+
+export default AddCart
